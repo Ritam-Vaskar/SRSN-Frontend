88fed808fddf467456ae11e8e6b6d37197981bc5
Ritam-Vaskar <vaskarananda77@gmail.com>
2024-08-24 21:28:07 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      // Add the module name(s) that should be externalized during the build
+      external: ['your-external-module'], // Replace with the actual module name
+    },
+  },
+});
